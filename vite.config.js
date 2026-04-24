cfc87b9aa9c3e67a2f1229d686a94b2e9bc9f00b
Will William <williamgitahi@gmail.com>
2026-04-25 01:11:53 +0300

    add netlify config for CSP fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false
+    sourcemap: false,
+    target: "esnext",
+    minify: "esbuild"
   }
 })
